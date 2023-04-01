de82c26ee4e7bba02897a27840ba54b95b05fdb6
Vasya <khitrik.rozanova.svetlana@gmail.com>
2023-04-02 01:47:27 +0300

    docs: update documentation for cd

--- commands/cd.hpp
+++ commands/cd.hpp
@@ -15,17 +15,23 @@ namespace  commands {
 class Cd: public Cmd {
 public:
     /**
-     * Print work directory to output stream
-     * How it is written in interpreter syntax:  <pwd>
+     * Change the working directory
+     * How it is written in interpreter syntax:  <cd path>
      *
-     * All tok.args are ignored
+     * <cd> without arguments leads to jumping to the root directory
+     *  '..' in the parameters means to go to the parent directory
+     *
+     *  example:
+     *  cd ..\\some_directory
+     *
      *
      * @param params: token with command name in tok.name and command arguments in tok.args
      * @param env: current environment variables of the interpreter
      * @param input: input FILE stream (unused)
      * @param output: output FILE stream
-     * @param err: error FILE stream (unused)
-     * @return 0 always
+     * @param err: error FILE stream
+     * @return 0 when it was possible to change the directory,
+     *         1 when errors occurred
      */
     virtual int run(const token& params, std::shared_ptr<Environment> env, FILE* input, FILE* output, FILE* err) override {
         std::stringstream result, error;
